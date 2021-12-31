0f5437b70f7a5c63885a460f812d7bc5a4f8909b
Smitha Jacob <smithatom78@gmail.com>
2021-12-31 10:32:43 +0530

    Update loginroute.js

--- src/routes/loginroute.js
+++ src/routes/loginroute.js
@@ -1,47 +1,47 @@
-const express = require('express'); 
+const express = require('express');
 const loginRouter = express.Router();
 const user = require('../data/user');
 
-loginRouter.get('/',function(req,res){
+loginRouter.get('/', function(req, res) {
 
-    res.render('login',{});
-    
+    res.render('login', {});
+
 })
 
 
-loginRouter.get("/check",function(req,res){
+loginRouter.get("/check", function(req, res) {
     var checkuser = {
-        uid:req.param("uid"),
-        pwd:req.param("pwd")
+        //uid: req.param("uid"),
+        uid: req.params("uid"),
+        pwd: req.params("pwd")
+            // pwd: req.param("pwd")
+            //part2#10. I am a deprecated method who welcomes a new user in this project.-param
     };
-    
+
     console.log(checkuser);
-    var flag=false;
+    var flag = false;
 
-//    var flagg = user.find((e)=>{
-       for(let i=0;i<user.length;i++){
-        
-        if(checkuser.uid==user[i].uid && checkuser.pwd==user[i].pwd)
-        {
-            
-            flag=true;
+    //    var flagg = user.find((e)=>{
+    for (let i = 0; i < user.length; i++) {
+
+        if (checkuser.uid == user[i].uid && checkuser.pwd == user[i].pwd) {
+
+            flag = true;
             break;
+        } else {
+            flag = false;
         }
-        else{
-                flag=false;
-            }
-        };
+    };
 
-        console.log(flag);
+    console.log(flag);
 
-if(flag==true){
-    // alert("User Verified.Click to continue");
-    res.redirect("/home")
-}
-else{
-    // alert("User Verification Failed");
-    res.redirect("/signup");
-}
+    if (flag == true) {
+        // alert("User Verified.Click to continue");
+        res.redirect("/home")
+    } else {
+        // alert("User Verification Failed");
+        res.redirect("/signup");
+    }
 
 });
 
