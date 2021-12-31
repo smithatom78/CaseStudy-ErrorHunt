cc51489b697e4228abdac9aa90f492df2d09950b
Smitha Jacob <smithatom78@gmail.com>
2021-12-31 11:22:04 +0530

    ch

--- app.js
+++ app.js
@@ -70,3 +70,5 @@ const favicon = require("serve-favicon");
 
 //var favicon = require('serve-favicon');
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
+
+//mongoose.set('useFindAndModify', false);
--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost:27017/Library');
-mongoose.connect('mongodb+srv://smithajacob:smithajacob@cluster0.1b8mq.mongodb.net/myFirstDatabase', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://smithajacob:smithajacob@cluster0.1b8mq.mongodb.net/myFirstDatabase', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 const Schema = mongoose.Schema;
 
 
